e53d303b3def7a486f0fe4b8d14233c2684a2295
Jul Jen <juliajen.prog@gmail.com>
2018-03-22 15:57:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 require_relative "../config/environment.rb"
 
 class Dog
@@ -56,9 +58,14 @@ class Dog
 
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
-
     result = DB[:conn].execute(sql, id)[0]
+
+binding.pry
     self.new(result[0], result[1], result[2])
+    
+    dog = Dog.new(row)
+    dog.save
+    dog
   end
 
 
