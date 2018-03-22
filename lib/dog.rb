26034080e4b00f34021c66c1c9e203ce0c765ff2
Jul Jen <juliajen.prog@gmail.com>
2018-03-22 14:55:19 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+require_relative "../config/environment.rb"
+
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(id:nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+end
