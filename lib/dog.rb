522d121675894e76311e72d3c01dab85bd6b214d
Jul Jen <juliajen.prog@gmail.com>
2018-03-22 16:07:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,9 +57,11 @@ class Dog
   end
 
   def self.find_by_id(id)
-    sql = "SELECT *
+    sql = <<-SQL
+    "SELECT *
     FROM dogs
     WHERE id = ?"
+    SQL
     result = DB[:conn].execute(sql, id)[0]
 
     dog = {}
