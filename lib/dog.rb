c522b94c03bba2889c71986b6ce559bc7d0f7237
Jul Jen <juliajen.prog@gmail.com>
2018-03-22 15:15:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,7 +27,18 @@ class Dog
   end
 
   def save
-  end 
+    # if self.id
+    #   self.update
+    # else
+      sql = <<-SQL
+        INSERT INTO dogs(name, breed)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    # end
+  end
 
 
   # expect(DB[:conn].execute ("SELECT * FROM dogs WHERE id = 1")).to eq([[1, "Teddy", "cockapoo"]])
